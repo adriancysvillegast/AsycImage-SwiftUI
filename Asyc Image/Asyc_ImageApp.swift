51304f7d1c4264fe44e5631e697c9124776dba1f
Adriancys Villegas <adriancysvillegast@gmail.com>
2023-02-27 13:38:32 -0400

    Initial Commit

---
+++ Asyc Image/Asyc_ImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asyc_ImageApp.swift
+//  Asyc Image
+//
+//  Created by Adriancys Jesus Villegas Toro on 27/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Asyc_ImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
